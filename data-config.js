dd5efc1c92feb1c7267b71dd5413f11ccff0a9d5
muriso1004-cyber <muriso1004@gmail.com>
2026-08-20 14:28:34 +0900

    feat: publish real estate website

---
+++ data-config.js
@@ -0,0 +1,11 @@
+// GitHub Actions가 생성하는 Raw JSON 주소를 관리자 화면에서 저장할 수 있습니다.
+// 저장된 브라우저 설정이 아래 기본값보다 우선합니다.
+window.DATA_CONFIG = {
+  repositoryFullName: 'muriso1004-cyber/sangdo15',
+  bundledSangdoUrl: 'assets/sangdo15-transactions-public.json',
+  kakaoChatUrl: 'https://open.kakao.com/o/sDcxMuoi',
+  noryangjinTransactionsUrl: '',
+  sangdoTransactionsUrl: '',
+  refreshMinutes: 30
+};
+
